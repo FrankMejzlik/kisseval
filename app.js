e95ffc8681e6b5879aa653388929fb8d27f224e3
devwesp <devwesp@gmail.com>
2019-04-25 19:50:05 +0200

    Add basic http authentication.

--- app.js
+++ app.js
@@ -42,6 +42,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+
+  // -----------------------------------------------------------------------
+  // authentication middleware
+
+  const auth = {login: 'dev', password: 'ndbi045'} // change this
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next()
+  }
+
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+  res.status(401).send('Authentication required.') // custom message
+
+  // -----------------------------------------------------------------------
+
+})
+
 app.use(session({ secret: 'matfyz', resave: false, saveUninitialized: true, }));
 
 

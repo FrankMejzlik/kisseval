357f561f8e74743a2078edf9dff9d4e219c589c3
devwesp <devwesp@gmail.com>
2019-05-18 12:08:16 +0200

    Implement bottom panel resize. Add distinc colours for charts.

--- routes/tests_ajax.js
+++ routes/tests_ajax.js
@@ -1,5 +1,6 @@
 
-const util = require('util')
+const util = require('util');
+const utils = require("../routes/utils/utils");
 
 var RunModelTest = function (aggFn, modelType, dataSource, settingsArray, aggSettingsArray) 
 {
@@ -37,99 +38,22 @@ exports.RunModelTest = function(req, res)
   const formId = req.query.submitData.formId;
   responseData.formId = formId;
 
-  for (var i = 0; i <formDataArray.length; ++i)
+  for (var i = 0; i < formDataArray.length; ++i)
   {
-    const aggFn = Number(formDataArray[i].aggregation);
-    const modelType = Number(formDataArray[i].modelType);
-    const dataSource = Number(formDataArray[i].dataSource);
-    const aggregationParam = String(formDataArray[i].aggregationParam);
-
-    const aggSettingsArray = new Array();
-    aggSettingsArray.push(aggregationParam);
-
-    // Variable with all settings
-    const settingsArray = new Array();
-
-    // Construct settings array based on model
-    switch (modelType)
-    {
-      /*!
-      * FORMAT:
-      *  0: Boolean:
-      *  1: BooleanBucket:
-      *    0 => trueTreshold
-      *    1 => inBucketSorting
-      *  2: BooleanExtended:
-      *  3: ViretBase:
-      *    0 => ignoreTreshold
-      *    1 => rankCalcMethod
-      *      0: Multiply & (Add |)
-      *      1: Add only
-      *  4: FuzzyLogic:
-      */
-
-      // Boolean
-      case 0:
-      // Nothing just yet
-      break;
-
-      // BooleanBucket
-      case 1:
-      {
-        
-        const keywordFrequency = formDataArray[i].keywordFrequency;
-        settingsArray.push(keywordFrequency);
-        // 0 => 
-        const probTreshold = formDataArray[i].trueTreshold;
-        settingsArray.push(probTreshold);
-
-        // 1 =>
-        const inBucketRanking = formDataArray[i].inBucketRanking;
-        settingsArray.push(inBucketRanking);
-      }
-      break;
-
-      // BooleanExtended
-      case 2:
-
-      break;
-
-      // ViretBase
-      case 3:
-      {
-        const keywordFrequency = formDataArray[i].keywordFrequency;
-        settingsArray.push(keywordFrequency);
-
-        // 0 =>
-        const probTreshold4 = formDataArray[i].trueTreshold4;
-        settingsArray.push(probTreshold4);
-
-        // 1 =>
-        const queryOperations = formDataArray[i].queryOperations;
-        settingsArray.push(queryOperations);
-      }
-      break;
-
-      // FuzzyLogic
-      case 4:
-
-      break;
-
-      default:
-        throw "Unknown model type.";
-    }
+    const nativeModelSettings = utils.convertSettingsObjectToNativeFormat(utils.parseModelSettingsFromForm(formDataArray[i]));
 
     // Run model test
-    const chartData = RunModelTest(aggFn, modelType, dataSource, settingsArray, aggSettingsArray);
+    const chartData = RunModelTest(
+      nativeModelSettings.aggregation,
+      nativeModelSettings.rankingModel, 
+      nativeModelSettings.dataSource, 
+      nativeModelSettings.rankingModelSettings, 
+      nativeModelSettings.aggregationSettings
+      );
 
     // Insert this result data to array
     responseData.chartDataArray.push(chartData);
   }
-
-  
-
-
-  
 
   // Send response
   res.jsonp(responseData);

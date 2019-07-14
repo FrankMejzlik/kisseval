24a61661ce822497ae3b62bdb332c611c1cc7b45
devwesp <devwesp@gmail.com>
2019-07-14 14:59:20 +0200

    Fix bug where task counter is not reset when page reloads.

--- routes/utils/trecvid_ranker_utils.js
+++ routes/utils/trecvid_ranker_utils.js
@@ -2,6 +2,41 @@ const fs = require('fs');
 
 const rankerUtils = require("./ranker_utils");
 
+/**
+ * Return a timestamp with the format "m/d/yy h:MM:ss TT"
+ * @type {Date}
+ */
+
+function timeStamp() {
+  // Create a date object with the current time
+    var now = new Date();
+  
+  // Create an array with the current month, day and time
+    var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+  
+  // Create an array with the current hour, minute and second
+    var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+  
+  // Determine AM or PM suffix based on the hour
+    var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+  
+  // Convert hour from military time
+    time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+  
+  // If hour is 0, set it to 12
+    time[0] = time[0] || 12;
+  
+  // If seconds and minutes are less than 10, add a zero
+    for ( var i = 1; i < 3; i++ ) {
+      if ( time[i] < 10 ) {
+        time[i] = "0" + time[i];
+      }
+    }
+  
+  // Return the formatted string
+    return date.join("-") + "_" + time.join("-");
+  }
+
 exports.initOrResumeTrecvidTaskSession = function(sess, viewData) 
 {
   global.logger.log('debug', "=> initOrResumeTrecvidTaskSession");
@@ -46,12 +81,10 @@ exports.startTrecvidTaskSession = function(sess)
 {
   global.logger.log('debug', "=> startTrecvidTaskSession()");
 
-  // Start new
-  if (typeof sess.trecvidSession === "undefined")
-  {
-    sess.trecvidSession = new Object();
-    sess.trecvidSession.currTaskIdx = 0;
-  }
+
+  sess.trecvidSession = new Object();
+  sess.trecvidSession.currTaskIdx = 0;
+
  
 
   var date = new Date();
@@ -92,9 +125,11 @@ exports.startTrecvidRun = function(sess, tasksFilepath, runType)
   // Start new
   sess.trecvidRun = new Object();
 
-  var date = new Date();
-  var timestamp = date.getTime();
-  sess.trecvidRun.outputFilepath = global.gConfig.outputPath + String(timestamp) + "_" + String(runType) + ".xml";
+  //var date = new Date();
+  //var timestamp = date.getTime();
+  var dateString = timeStamp();
+
+  sess.trecvidRun.outputFilepath = global.gConfig.outputPath + String(dateString) + "_" + String(runType) + ".xml";
 
   sess.trecvidRun.runType = runType;
   sess.trecvidRun.tasks = tasks;
@@ -132,7 +167,8 @@ exports.terminateTrecvidRun = function(sess)
 exports.printXmlHeader = function(sess)
 {
   let headerString = 
-    '<videoAdhocSearchResults>\n\
+    '<!DOCTYPE videoAdhocSearchResults SYSTEM "https://www-nlpir.nist.gov/projects/tv2019/dtds/videoAdhocSearchResults.dtd">\n\n\
+    <videoAdhocSearchResults>\n\
     \t<videoAdhocSearchRunResult trType="A" class="M" task="'+ sess.trecvidRun.runType + '" novelty="C" pid="SIRET" priority="1" desc="">\n';
 
   // Append to file
@@ -153,7 +189,9 @@ exports.printXmlShots = function(sess, elapsedTime, shots)
 {
   const taskId = sess.trecvidRun.tasks[sess.trecvidSession.currTaskIdx].id;
 
-  let totalString = '\t\t <videoAdhocSearchTopicResult tNum="' + taskId + '" elapsedTime="' + elapsedTime + '">\n';
+  const elapsedTimeRounded = Math.round(elapsedTime * 10) / 10;
+
+  let totalString = '\t\t <videoAdhocSearchTopicResult tNum="' + taskId + '" elapsedTime="' + elapsedTimeRounded + '">\n';
 
   for (i in shots)
   {

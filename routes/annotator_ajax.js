9d03e39a94ed8d724d1926bd6d3fab19c33a45f6
devwesp <devwesp@gmail.com>
2019-09-23 18:00:42 +0200

    Add logging of prefixes required with autocomplete. Store them in files based on keyword type that is being used right now.

--- routes/annotator_ajax.js
+++ routes/annotator_ajax.js
@@ -1,5 +1,30 @@
 
-const util = require('util')
+const util = require('util');
+const fs = require("fs");
+var path = require('path');
+
+let prefixCache = new Array();
+
+function writePrefixCache(kwDataType)
+{
+  let origArr = prefixCache;
+  prefixCache = new Array();
+
+  var today = new Date();
+  let todayString = String(today.getDate()) + "-" + String(today.getMonth()) + "-" + String(today.getFullYear());
+
+  const filepath = path.join(global.rootDir, global.gConfig.outputDir + '/autocomplete/' + todayString + "." + kwDataType +  ".log");
+
+  let file = fs.createWriteStream(filepath, {flags: 'a'});
+  file.on('error', function(err) 
+  { 
+    return;
+  });
+  origArr.forEach(function(v) { file.write(v + '\n'); });
+  file.end();
+
+  
+}
 
 exports.GetNearKeywords = function(req, res) 
 {
@@ -11,6 +36,12 @@ exports.GetNearKeywords = function(req, res)
   const withExamples = sess.annotatorSettings.autocompleteWithExamples;
 
   var nearKeywords = global.imageRanker.GetNearKeywords(kwDataType, scoringDataType, prefix, sess.annotatorSettings.numSuggestions, withExamples);
+
+  prefixCache.push(prefix);
+  if (prefixCache.length > 100)
+  {
+    writePrefixCache(kwDataType);
+  }
 
   // Send response
   res.jsonp(nearKeywords);

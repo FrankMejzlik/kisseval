fedcdfe4cb9d986db1eb91c1ab44df5d25de127b
devwesp <devwesp@gmail.com>
2019-09-24 10:06:56 +0200

    Add check if provided keyword and scoring datat types are compatible.

--- routes/settings_ajax.js
+++ routes/settings_ajax.js
@@ -3,10 +3,26 @@ const util = require('util')
 
 exports.SetKeywordScoringDataType = function(req, res) 
 {
+  
   let sess = req.session;
+  const a = Number(req.body.kwDataType);
+  const b = Number(req.body.scoringDataType);
+  
 
-  sess.keywordsSettings.kwDataType = Number(req.body.kwDataType);
-  sess.rankingSettings.scoringDataType = Number(req.body.scoringDataType);
+  // Check if this pair is compatible
+  // 0000001|00 - last to digits determine DCNN type
+  // every digit position shows what keywords it is
+  const aa = a / 100;
+  const bb = b / 100;
+  if (aa != bb)
+  {
+    global.logger.log('warn', "Incompatigle keyword and scoring data type: (" + a + ", " + b + ") \n Settings stay unchanged.");
+    res.jsonp("Incompatigle keyword and scoring data type: (" + a + ", " + b + ")");
+  }
 
+  sess.keywordsSettings.kwDataType = a;
+  sess.rankingSettings.scoringDataType = b;
+
+  global.logger.log('info', "Keyword and scoring type changed to (" + a + ", " + b + ")");
   res.jsonp(true);
 };

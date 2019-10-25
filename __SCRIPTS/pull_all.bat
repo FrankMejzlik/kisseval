d7dc283a0de7d490e3e014a8104c15da8bbd8731
Frank Mejzlik <frankmejzlik@gmail.com>
2019-10-25 18:18:04 +0200

    Edit pull-all.bat script.

--- __SCRIPTS/pull_all.bat
+++ __SCRIPTS/pull_all.bat
@@ -1,8 +1,24 @@
-ECHO Pulling this repo with all subrepos
+@echo off
 
-cd ..
-git pull
+echo Pulling this repo with all subrepos
+
+set BASE_DIR=.
+
+set FRONTEND_REMOTE=origin
+set FRONTEND_BRANCH=dev
+set BACKEND_REMOTE=origin
+set BACKEND_BRANCH=dev
+
+cd %BASE_DIR%\..
+
+git checkout -- .
+git checkout %FRONTEND_BRANCH%
+git pull %FRONTEND_REMOTE% %FRONTEND_BRANCH%
+
 cd .\cppsrc\ImageRankerWrapper\ImageRanker\
-git pull 
+git checkout -- .
+git checkout %BACKEND_BRANCH%
+git pull %BACKEND_REMOTE% %BACKEND_BRANCH%
 
-cmd /k
+echo Done!
+pause
